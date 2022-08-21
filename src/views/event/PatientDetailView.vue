fa22ae5963ca36930b25e31fc92eab35badc0f35
vanida gowan <vanidagowan@gmail.com>
2022-08-21 20:57:44 +0700

    Add content to PatientDetailView.vue

---
+++ src/views/event/PatientDetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <img :src="patient.image" class="image" />
+    <h4>Name: {{ patient.name }} {{ patient.surname }}</h4>
+    <p>Age: {{ patient.age }}</p>
+    <p>Gender: {{ patient.gender }}</p>
+    <p>Hometown: {{ patient.hometown }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ['patient']
+}
+</script>
