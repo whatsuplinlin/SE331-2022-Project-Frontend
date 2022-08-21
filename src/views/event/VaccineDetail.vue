7e9adc09da9433d00489d25c179b224a21efa9c1
whatsuplinlin <kjm010704@gmail.com>
2022-08-21 20:59:56 +0700

    Add content to VaccineDetail.vue

---
+++ src/views/event/VaccineDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <table>
+      <tr>
+        <th>DOSE NO.</th>
+        <th>TYPE</th>
+        <th>DATE</th>
+      </tr>
+      <tr v-for="vaccine in vaccines.details" :key="vaccine">
+        <td>{{ vaccine.number }}</td>
+        <td>{{ vaccine.type }}</td>
+        <td>{{ vaccine.date }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: ['vaccines']
+}
+</script>
