fb518c54876218dd5365bb7c46efc29c52ff2af2
whatsuplinlin <kjm010704@gmail.com>
2022-08-21 21:44:37 +0700

    Change the style of VaccineDetail.vue

--- src/views/event/VaccineDetail.vue
+++ src/views/event/VaccineDetail.vue
@@ -19,3 +19,24 @@ export default {
   props: ['vaccines']
 }
 </script>
+<style scoped>
+table {
+  margin: auto;
+  margin-top: 100px;
+}
+
+th {
+  width: 230px;
+  color: #eff9fe;
+  font-weight: 100;
+  background: #435d45;
+  padding: 20px;
+  text-align: center;
+}
+
+td {
+  background: #eff9fe;
+  border-top: dotted 1px #435d45;
+  padding: 20px;
+}
+</style>
