f1d05863b38c78b5353796241a2f6181d7203c27
vanida gowan <vanidagowan@gmail.com>
2022-08-22 00:51:28 +0700

    Fix error page design

--- src/views/NetworkError.vue
+++ src/views/NetworkError.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="networkError">
     <img
-      src="https://images-ext-1.discordapp.net/external/7CjJpwwtNFVBPN0UcR0IzIi91xWvywhyN3haTD_OljM/%3Fk%3D20%26m%3D1301337122%26s%3D612x612%26w%3D0%26h%3DREAzf6wr2yDFMIUTymYzHbG2zjEsyjOuTapwSo4q9Rw%3D/https/media.istockphoto.com/vectors/internet-not-connected-icon-vector-id1301337122"
+      src="https://static.thenounproject.com/png/207492-200.png"
       class="image"
     />
-    <h2>NETWORK ERROR❗️</h2>
-    <h3>Please check your internet connection</h3>
-    <h3><a href="#" @click="$router.go(-1)">Click Here To Try Again</a></h3>
+    <h2>NETWORK ERROR❕</h2>
+    <br />
+    <h4>Please check your internet connection</h4>
+    <p><a href="#" @click="$router.go(-1)">Click Here To Try Again</a></p>
   </div>
 </template>
 <style scoped>
@@ -17,10 +18,14 @@
 }
 
 h2 {
-  color: red;
+  color: white;
 }
 
-.h3,
+p {
+  font-size: 18px;
+}
+
+.h4,
 a {
   color: black;
   text-decoration: none;
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <img
-      src="https://www.vkf-renzel.com/out/pictures/generated/product/1/650_650_75/r12044336-01/general-warning-sign-10836-1.jpg"
+      src="https://cdn.pixabay.com/photo/2014/04/02/10/24/warning-303768_960_720.png"
       class="image"
     />
-    <h2>DATA DOES NOT EXIST❗️</h2>
+    <h2>DATA DOES NOT EXIST❕</h2>
     <router-link :to="{ name: 'home' }" class="back">Back to Home</router-link>
   </div>
 </template>
 <style scoped>
 .image {
-  width: 250px;
+  width: 280px;
   height: 250px;
   margin: 50px;
 }
 
 h2 {
-  color: red;
+  color: white;
 }
 
 .back {
