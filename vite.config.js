14f7b6610bb6d42fc48b8b309cfccc29c2190ab6
whatsuplinlin <kjm010704@gmail.com>
2022-08-22 19:47:26 +0700

    Delete vite file

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/SE331-2022-Project-1/',
-  plugins: [vue()]
-})
